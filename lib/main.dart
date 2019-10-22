cd14654bf377181dd44d8bb08bc12d0ed7403694
Pablo Souza <pvrsouza@gmail.com>
2019-10-22 16:17:26 -0300

    centralizando rotas em um arquivo de rotas

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/category_meals_screen.dart';
+
+import './category_meals_screen.dart';
 import './categories_screen.dart';
+import './routes.dart';
 
 void main() => runApp(MyApp());
 
@@ -29,9 +31,7 @@ class MyApp extends StatelessWidget {
             ),
       ),
       home: CategoriesScreen(),
-      routes: {
-        '/categories-meals': (ctx) => CategoryMealsScreen(),
-      },
+      routes: ROUTES,
     );
   }
 }
---
+++ lib/routes.dart
@@ -0,0 +1,5 @@
+import './category_meals_screen.dart';
+
+final ROUTES = {
+  '/categories-meals': (ctx) => CategoryMealsScreen(),
+};
